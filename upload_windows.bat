84126223ed48e0f573e5197935e85f8518ddcd83
Juan C. Roldán <juancarlos@sevilla.es>
2021-04-30 10:07:39 +0200

    Added pypi deploy script

---
+++ upload_windows.bat
@@ -0,0 +1,16 @@
+rm build -R
+rm dist -R
+rm autoselenium.egg-info -R
+rm autoselenium-* -R
+read -p "Please, open setup.py and update the version."
+git add .
+git commit
+git push origin master
+python setup.py sdist bdist_wheel
+python -m twine upload dist/*
+pip uninstall autoselenium
+pip install autoselenium -U
+rm build -R
+rm dist -R
+rm autoselenium.egg-info -R
+rm autoselenium-* -R
