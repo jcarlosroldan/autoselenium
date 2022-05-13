761731e73c3846bf3cc96019592e22bad08ce446
Juan C. Roldán <juancarlos@sevilla.es>
2022-05-13 11:18:34 +0200

    Improved uploader

--- upload_windows.bat
+++ upload_windows.bat
@@ -1,16 +1,31 @@
-rm build -R
-rm dist -R
-rm autoselenium.egg-info -R
-rm autoselenium-* -R
-read -p "Please, open setup.py and update the version."
-git add .
+@ECHO OFF
+
+Rem Remove auxiliary files
+rd /s /q build 
+rd /s /q dist
+rd /s /q autoselenium.egg-info
+for /d %%x in (autoselenium-*) do rd /s /q %%x
+
+Rem Commit changes
+git add *
 git commit
 git push origin master
+
+Rem Update library
 python setup.py sdist bdist_wheel
-python -m twine upload dist/*
-pip uninstall autoselenium
+python -m twine upload dist/* -u juancroldan
+
+Rem Remove auxiliary files again
+rd /s /q build 
+rd /s /q dist
+rd /s /q autoselenium.egg-info
+for /d %%x in (autoselenium-*) do rd /s /q %%x
+
+timeout /t 5
+
+Rem Reinstall it
+ipconfig /flushdns
+pip uninstall autoselenium -y
 pip install autoselenium -U
-rm build -R
-rm dist -R
-rm autoselenium.egg-info -R
-rm autoselenium-* -R
+
+pause
